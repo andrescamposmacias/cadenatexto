99aaf825c5759eac2ac961c6e4cfdface5c4828b
Andres <andrescamposmacias@gmail.com>
2019-01-10 13:34:15 +0100

    Metodos substring() y concat()

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -10,16 +10,34 @@ package clasestring;
  * @author andres
  */
 public class ClaseString {
-    
+
     public static void main(String[] args) {
-        
+
         String s = "iesmardealboran.com";
-        
+
         //tamaño de la cadena
         System.out.println("Tamaño: " + s.length());
-        
+
         //obtener el char de una posicion concreta
         System.out.println("Char de la posicion 5: " + s.charAt(5));
+
+        //obtener subcadena desde el caracter i-esimo hasta el final
+        //de la cadena
+        String subcadena = s.substring(3);
+        System.out.println("Subcadena = " + subcadena);
+
+        //obtener subcadena desde el caracter i-esimo hasta el
+        //caracter j-esimo de la cadena
+        subcadena = s.substring(8, 15);
+        System.out.println("Subcadena = " + subcadena);
+        
+        //concatenar cadenas, equivale al operador +
+        String s1 = "ies";
+        String s2 = "mardealboran.com";
+        String nuevo = s1 + s2;
+        String nuevo2 = s1.concat(s2);
+        System.out.println("Nuevo: " + nuevo);
+        System.out.println("Nuevo2: " + nuevo2);
     }
-    
+
 }
