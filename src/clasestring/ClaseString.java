d5fdf8ecce8f085f0a0958a2379dbd175203d6ff
Andres <andrescamposmacias@gmail.com>
2019-01-10 13:45:52 +0100

    Metodos equals() e IndexOf

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -38,6 +38,19 @@ public class ClaseString {
         String nuevo2 = s1.concat(s2);
         System.out.println("Nuevo: " + nuevo);
         System.out.println("Nuevo2: " + nuevo2);
+        
+        //devuelve el indice dentro de la cadena
+        //de la primera aparicion de la cadena a buscar
+        int indice = nuevo.indexOf("mar");
+        System.out.println("Indice: " + indice);
+        
+        indice = nuevo.indexOf("ran", 10);
+        System.out.println("Indice: " + indice);
+        
+        //metodos equals y equalsIgnoreCase
+        System.out.println("Igualdad Java y java: " + "Java".equals("java"));
+        System.out.println("Igualdad Java y java: " + "Java".equalsIgnoreCase("java"));
+        
     }
 
 }
